820bf46f0c12959fea81e1e77868ad5587e313ab
Alexizhere <alexizarand1@gmail.com>
2024-05-11 09:46:33 +0200

    Scoring implemented

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -17,20 +17,6 @@ public class Ball : MonoBehaviour
         ResetPosition();
     }
 
-    private void AddStartingforce()
-    {
-        float x = Random.value < 0.5f ? -1.0f : 1.0f;
-        float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : 
-                                        Random.Range(0.5f, 1.0f);
-        Vector2 direction = new Vector2(x, y);
-        _rigidbody.AddForce(direction * speed);
-    }
-
-    public void AddForce(Vector2 force)
-    {
-        _rigidbody.AddForce(force);
-    }
-
     public void ResetPosition()
     {
         _rigidbody.position = Vector3.zero;
@@ -38,4 +24,20 @@ public class Ball : MonoBehaviour
 
         AddStartingforce();
     }
+
+    private void AddStartingforce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : 
+                                        Random.Range(0.5f, 1.0f);
+        Vector2 direction = new Vector2(x, y);
+        _rigidbody.AddForce(direction * this.speed);
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        _rigidbody.AddForce(force);
+    }
+
+
 }
